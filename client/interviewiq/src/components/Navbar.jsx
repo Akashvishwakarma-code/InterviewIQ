7c29faecdf21545bc12011172d56b4280fa5d4bd
Akash vishwakarma <vishwakarmaakashrjcollege@gmail.com>
2026-08-03 22:37:31 +0530

    feat(authModel): add authmodel and logout

--- client/interviewiq/src/components/Navbar.jsx
+++ client/interviewiq/src/components/Navbar.jsx
@@ -3,8 +3,13 @@ import { motion } from "motion/react"
 import { BsCoin, BsRobot } from 'react-icons/bs'
 import { FaUserAstronaut } from "react-icons/fa"
 import { HiOutlineLogout } from "react-icons/hi"
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
+import { setUserData } from '../redux/userSlice.js'
+import AuthModel from './AuthModel'
+import axios from "axios"
+import { ServerUrl } from '../App.jsx'
+
 
 function Navbar() {
 
@@ -12,6 +17,23 @@ function Navbar() {
     const [showCreditPopup, setShowCreditPopup] = useState(false)
     const [showUserPopup, setShowUserPopup] = useState(false)
     const navigate = useNavigate()
+    const dispatch =useDispatch()
+    const [showAuth ,setShowAuth]= useState(false);
+
+    const handleLogout = async () =>{
+        try{
+            await axios.post(ServerUrl + "/api/auth/logout",
+                {},
+                {withCredentials:true}
+            )
+            dispatch(setUserData(null))
+            setShowCreditPopup(false)
+            setShowUserPopup(false)
+            navigate("/")
+        }catch(error){
+            console.log(error)
+        }
+    }
   return (
     <div className= "bg-[#f3f3f3] flex justify-center px-4 pt-6">
         <motion.div
@@ -27,14 +49,18 @@ function Navbar() {
             </div>
             <div className="flex items-cetner gap-6 relative ">
                 <div className="relative">
-                    <button onClick={() => {setShowCreditPopup(!showCreditPopup);
+                    <button onClick={() => {
+                        if(!userData){setShowAuth(true)
+                            return;
+                        }
+                        setShowCreditPopup(!showCreditPopup);
                         setShowUserPopup(false)
                     }} className="flex items-center gap-2 bg-gray-100 px-4 py-2 rounded-full text-md hover:bg-gray-200 transition ">
                         <BsCoin size={20} />
                         {userData?.credits || 0}
 
                     </button>
-                    {showCreditPopup && (<div className="absolute right-[-50px] mt-3 w-64 bg-white shadow-xl border border-gray-200 rounded p-5 z-50">
+                    {showCreditPopup && (<div className="absolute right-0 mt-3 w-64 bg-white shadow-xl border border-gray-200 rounded p-5 z-50">
                             <p className="text-sm text-gray-600 mb-4">
                                 Need more Credits to continue Interviews?
                             </p>
@@ -43,7 +69,9 @@ function Navbar() {
                     )}
                 </div>
                 <div className="relative">
-                    <button onClick={() => {setShowUserPopup(!showUserPopup); setShowCreditPopup(false)}} className="w-9 h-9 bg-black text-white rounded-full flex items-center justify-center font-semibold">
+                    <button onClick={() => {if(!userData){setShowAuth(true)
+                            return;
+                        }setShowUserPopup(!showUserPopup); setShowCreditPopup(false)}} className="w-9 h-9 bg-black text-white rounded-full flex items-center justify-center font-semibold">
                         {userData ? userData.name.slice(0, 1).toUpperCase() : <FaUserAstronaut size={16} />}
                     </button>
                     {showUserPopup && (<div className="absolute right-0 mt-3 bg-white shadow-xl border border-gray-200 rounded-xl p-4 z-50">
@@ -51,7 +79,7 @@ function Navbar() {
                             {userData?.name}
                         </p>
                         <button onClick={() => navigate("/history")} className="w-full text-left text-sm py-2 hover:text-black text-gray-600">InterView History</button>
-                        <button className="w-full text-left text-sm py-2 flex items-center gap-2 text-red-500">
+                        <button onClick={handleLogout} className="w-full text-left text-sm py-2 flex items-center gap-2 text-red-500 cursor-pointer">
                             <HiOutlineLogout size={16}/> Logout
                         </button>
                     </div>)}
@@ -59,6 +87,7 @@ function Navbar() {
                 </div>
             </div>
         </motion.div>
+        {showAuth && <AuthModel onClose={()=>setShowAuth(false)}/>}
       
     </div>
   )
--- client/interviewiq/src/pages/Home.jsx
+++ client/interviewiq/src/pages/Home.jsx
@@ -1,12 +1,80 @@
-import React from 'react'
-import { BsRobot } from "react-icons/bs";
+import React, { useState } from 'react'
 import Navbar from '../components/Navbar.jsx';
-
+import {motion} from "motion/react"
+import { BsRobot,BsMic, BsClock, BsBarChart } from "react-icons/bs"
+import {HiSparkles} from "react-icons/hi"
+import { useSelector } from 'react-redux';
+import AuthModel from '../components/AuthModel.jsx';
+import { useNavigate } from 'react-router-dom';
 function Home() {
+  const {userData} = useSelector((state)=>state.user)
+  const [showAuth,setShowAuth] =useState(false);
+  const navigate =useNavigate()
   return (
-    <div className="min-h-screen bg-[#f3f3f3] flex flex-col" >
-      <Navbar/>
-       </div>
+    <div className="min-h-screen bg-[#f3f3f3] overflow-x-hidden" >
+      <div className="w-full max-w-screen-xl mx-auto flex flex-col">
+        <Navbar/>
+
+        <main className="flex-1 px-4 py-10 lg:px-6">
+          <div className="flex w-fit mx-auto bg-gray-100 text-gray-600 text-sm px-4 py-2 rounded-full justify-center items-center gap-2">
+            <HiSparkles size={16} className="bg-green-50 text-green-600" />
+            AI Powered Smart Interview Platform
+          </div>
+
+          <div className="text-center mb-10 mt-5">
+          <motion.h1
+          initial ={{opacity:0,y:30}}
+          animate={{opacity:1,y:0}}
+          transition={{duration:0.6}}
+           className="text-4xl md:text-6xl font-semibold leading-tight max-w-4xl mx-auto">
+            Practice Interviews with 
+            <span className="relative inline-block">
+              <span className="bg-green-100 text-green-600 px-5 py-1 rounded-full">
+                AI Intelligence
+              </span>
+            </span>
+          </motion.h1>
+          
+          <motion.p
+          initial={{opacity:0}}
+          animate={{opacity:1}}
+          transition={{duration:0.8}}
+           className="text-gray-500 mt-6 max-w-2xl mx-auto text-lg">
+            Role-based mock interviews with smart follow-ups,adaptive difficulty and real-time perfromance evaluation.
+          </motion.p>
+          <div className="flex flex-wrap justify-center gap-4 mt-10">
+            <motion.button 
+            onClick={()=>{
+              if(!userData){
+                setShowAuth(true)
+                return;
+              }
+              navigate("/interview")
+            }}
+            whileHover={{opacity:0.9,scale:1.03}}
+            whileTrap={{opacity:1,scale:0.98}}
+            className="bg-black text-white px-10 py-3 rounded-full hover:opacity-90 transition shadow-md">
+              Start Interview
+            </motion.button>
+            <motion.button 
+            onClick={()=>{
+              if(!userData){
+                setShowAuth(true)
+                return;
+              }
+              navigate("/history")
+            }}
+            whileHover={{opacity:0.9,scale:1.03}}
+            whileTrap={{opacity:1,scale:0.98}}
+            className="border border-gray-300 px-10 py-3 rounded-full hover:bg-gray-100 transition">
+                View History            
+          </motion.button>
+          </div>
+          </div>
+        </main>
+        </div>
+        {showAuth && <AuthModel onClose={()=>setShowAuth(false)}/>}
+    </div>
   )
 }
 
