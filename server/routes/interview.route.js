38880ddc4d931b0176aa08acac32bce212eff56d
Akash vishwakarma <vishwakarmaakashrjcollege@gmail.com>
2026-09-13 20:40:45 +0530

    interview roue updated with question, submitanswer, finishInterview routes

--- server/routes/interview.route.js
+++ server/routes/interview.route.js
@@ -1,7 +1,7 @@
 import express from "express"
 import isAuth from "../middleware/isAuth.js"
 import { upload } from "../middleware/multer.js"
-import { analyzeResume } from "../controllers/interview.controller.js"
+import { analyzeResume, finishInterview, generateQuestion, submitAnswer } from "../controllers/interview.controller.js"
 
 const interviewRouter = express.Router()
 
@@ -13,5 +13,9 @@ interviewRouter.post("/resume", isAuth, (req, res, next) => {
         next();
     });
 }, analyzeResume);
+
+interviewRouter.post("/generate-questions",isAuth,generateQuestion)
+interviewRouter.post("/submit-answer",isAuth,submitAnswer)
+interviewRouter.post("/finish",isAuth,finishInterview)
 
 export default interviewRouter
